4b829eb728e3b2d256e7784ccb2ba7e7c9b27967
Syed Nouman <aazi.razan@gmail.com>
2024-04-20 01:29:26 +0530

    cert

--- components/designs/More.tsx
+++ components/designs/More.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function More() {
     return (
       <p className="py-20 text-sm italic text-fun-gray">
-        That's not all... I've got more on <a className="text-fun-pink underline" href="https://dribbble.com/braydentw" target="_blank">my Dribbble</a>!
+        That's not all... I've got <a className="text-fun-pink underline" href="https://drive.google.com/drive/folders/1D-es07uXr06scPh0FTi8hwh4SmPZOpHY?usp=drive_link" target="_blank">more</a>!
       </p>
     );
 }
--- data/content/designs.ts
+++ data/content/designs.ts
@@ -7,42 +7,42 @@ type Design = {
 export const designs: Design[] = [
   {
     label: "Mobile Shopping App",
-    img: "/static/designs/SixSigmaWhiteBelt.webp",
-    link: "https://dribbble.com/shots/9458498-Mobile-Shopping-App",
+    img: "/static/designs/Sigma White belt.webp",
+    link: "https://drive.google.com/file/d/1na4qPBcZ0R-JdF9EqNkD48YmDQVsQ5m6/view?usp=drive_link",
   },
   {
     label: "Modern App Design",
-    img: "/static/designs/GenAIforPM.webp",
-    link: "https://dribbble.com/shots/15485626-Modern-App-Design",
+    img: "/static/designs/Genrative AI PM.webp",
+    link: "https://drive.google.com/file/d/1ioU01d1mZuQbztQBUSaW3tIThQfiMGyN/view?usp=drive_link",
   },
   {
     label: "Landing Pages for Startups - Concept",
-    img: "/static/designs/UXDesignCalArts.webp",
-    link: "https://dribbble.com/shots/15485635-Landing-Pages-for-Startups-Concept",
+    img: "/static/designs/Cal arts.webp",
+    link: "https://drive.google.com/file/d/1ZRKsQDj1SpMv859OPueHNJ7GYcwXplEM/view?usp=sharing",
   },
   {
     label: "Luxury, Blue Portfolio",
-    img: "/static/designs/GoogleDigitalMarketing.webp",
-    link: "https://dribbble.com/shots/15165547-Luxury-Blue-Portfolio",
+    img: "/static/designs/Google Digital Marketing .webp",
+    link: "https://drive.google.com/file/d/13axj9zgB2yvGL21ns-PSVtbpXRbALdms/view?usp=sharing",
   },
   {
     label: "Mobile Deal Page",
-    img: "/static/designs/mobile-deal-page.jpg",
-    link: "https://dribbble.com/shots/9462248-Mobile-Deal-Page",
+    img: "/static/designs/Mohamed Aasim K rising award.webp",
+    link: "https://drive.google.com/file/d/1NE2JVpfoSOUla4EjbLn7N4VPhNEIByi3/view?usp=drive_link",
   },
   {
     label: "Neon Signatures",
-    img: "/static/designs/neon-signatures.jpeg",
-    link: "https://dribbble.com/shots/11134809-Neon-Signatures",
+    img: "/static/designs/Adversting with meta SM.webp",
+    link: "https://drive.google.com/file/d/17jp8qhZdTaeudyZ09MiHyiwSGM34O5kD/view?usp=drive_link",
   },
   {
     label: "The Dentista - Logo Design",
-    img: "/static/designs/the-dentista-logo.png",
-    link: "https://dribbble.com/shots/14571857-The-Dentista-Logo-Design",
+    img: "/static/designs/Georgia Tech certificatesn.webp",
+    link: "https://drive.google.com/file/d/1q-HN_flwvxXxwEHkfbCXqmv38R1xOXAq/view?usp=drive_link",
   },
   {
     label: "iPhone Wallpapers",
-    img: "/static/designs/iphone-wallpapers.png",
-    link: "https://dribbble.com/shots/14077089-Free-iPhone-Wallpapers",
+    img: "/static/designs/Google Project Management.webp",
+    link: "https://drive.google.com/file/d/1B6dvtf4SlSkePUhJ5YBaRMTbp2OfTAXk/view?usp=drive_link",
   },
 ];
