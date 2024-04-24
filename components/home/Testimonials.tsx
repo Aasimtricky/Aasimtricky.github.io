1394eb0f16ff505f93ebfe3bd4b4066c2f8cf1e9
Syed Nouman <aazi.razan@gmail.com>
2024-04-25 01:06:52 +0530

    Update Testimonials.tsx

--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -16,11 +16,8 @@ function About() {
         <div className="md:flex-1 bg-fun-pink border border-fun-pink-light rounded-lg p-5 flex flex-col justify-center">
           <p className="text-base">
           I'm currently based in New York, exploring career opportunities across the USA. With extensive experience in project management and UI/UX design, I have successfully led a variety of projects in sectors ranging from websites and games to healthcare and e-commerce. I am also advancing my skills through a Master’s in Social Media and Mobile Marketing at Pace University.          </p>
-          <p className="mt-4 text-xs text-fun-gray">
+          <p className="mt-4 text-base text-black">
           Beyond the technical expertise, my real passion lies in connecting with people. Whether it’s by creating memorable product experiences or simply sharing a moment of joy, I value every interaction. My approach to product management is driven by an insatiable curiosity, deep empathy, and an eagerness to continuously learn and grow. In my free time, I enjoy playing cricket, which not only keeps me active but also sharpens my team-building skills. Let's connect and make something impactful together!
-          </p>
-          <p className="mt-2 text-xs text-fun-gray">
-            I'm always open to discussing new projects or opportunities to collaborate. Feel free to reach out!
           </p>
         </div>
       </div>
