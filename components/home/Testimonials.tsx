7f89c9d38d764486b5db44926ea923978d2c31b6
Aasimtricky <82271411+Aasimtricky@users.noreply.github.com>
2024-08-22 15:51:21 +0530

    Update Testimonials.tsx

--- components/home/Testimonials.tsx
+++ components/home/Testimonials.tsx
@@ -21,7 +21,7 @@ function About() {
           </p>
           <button
             className="mt-4 py-2 px-5 bg-fun-gray-dark hover:bg-fun-gray-darkest text-white font-bold rounded-lg transition-colors"
-            onClick={() => window.location.href='/static/misc/resume.pdf'} // Update with the actual path to your resume
+            onClick={() => window.location.href='https://docs.google.com/document/d/1MSwNVSOedE1PKP3OvZlPvBHaXU3J7ldM/edit?usp=sharing&ouid=106726599693480326044&rtpof=true&sd=true'} // Update with the actual path to your resume
           >
             Download Resume
           </button>
